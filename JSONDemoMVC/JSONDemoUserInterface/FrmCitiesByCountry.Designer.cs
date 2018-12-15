3529b48f79976a1bd7881f8d1cdd6b9861e9349e
maxmart97 <maxmart97@gmail.com>
2018-12-15 11:04:13 -0600

    Renamed some files and deleted some that were not needed

--- JSONDemoMVC/JSONDemoUserInterface/FrmCitiesByCountry.Designer.cs
+++ JSONDemoMVC/JSONDemoUserInterface/FrmCitiesByCountry.Designer.cs
@@ -61,7 +61,7 @@
             this.btnExit.TabIndex = 1;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
             // 
             // cboCountries
             // 
